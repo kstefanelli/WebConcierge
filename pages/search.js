99188e2631483a3b6317633249df5bf22db5d86c
Kristina Stefanelli <kristinastefanelli@gmail.com>
2022-04-01 15:26:34 -0400

    fix: Updated Server Side Render Function

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import { API_KEY, CONTEXT_KEY } from '../keys'
+import { API_KEY, CONTEXT_KEY } from '../keys';
+import Response from '../Response'
 
 function Search({ results }) {
-  console.log(results)
+  console.log(results);
   return (
     <div>
       <Head>
@@ -12,22 +13,23 @@ function Search({ results }) {
       </Head>
       <Header />
     </div>
-  )
+  );
 }
 
 export default Search;
 
-export async function getServerSideProps(context){
-  const useDummyData = false;
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const startIndex = context.query.start || '0';  
 
-  const data = await fetch(
-    `https://wwww.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
-    ).then((response) => response.text())
-
+  const data = useDummyData ? Response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}
+    &q=${context.query.term}&start=${startIndex}`
+  ).then((response) => response.json());
 
   return {
     props: {
       results: data,
     },
-  }
+  };
 }
